9c8e31e1292020dfd657a2cecac7629ca65e0d64
stefan-wrapper <stefan.stankovic@ncoded.solutions>
2024-08-15 16:45:35 +0200

    Remove tests

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
 import { RegisterUserDto } from './dtos/register-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
